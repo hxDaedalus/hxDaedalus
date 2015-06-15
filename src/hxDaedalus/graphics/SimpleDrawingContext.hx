3799e203ee0e20b9e1d217c13d3ce109520fe8fe
Justin Mills <jlm@justinfront.net>
2015-06-15 15:23:27 +0100

    Added svg drawing support.

--- src/hxDaedalus/graphics/SimpleDrawingContext.hx
+++ src/hxDaedalus/graphics/SimpleDrawingContext.hx
@@ -2,10 +2,12 @@ package hxDaedalus.graphics;
 
 #if flambe
 typedef SimpleDrawingContext = hxDaedalus.graphics.flambe.SimpleDrawingContext;
-#elseif (flash || openfl || nme) 
+#elseif (flash || openfl || nme)
 typedef SimpleDrawingContext = hxDaedalus.graphics.flash.SimpleDrawingContext;
 #elseif luxe
 typedef SimpleDrawingContext = hxDaedalus.graphics.luxe.SimpleDrawingContext;
+#elseif svg
+typedef SimpleDrawingContext = hxDaedalus.graphics.svg.SimpleDrawingContext;
 #elseif js
 typedef SimpleDrawingContext = hxDaedalus.graphics.js.SimpleDrawingContext;
 #elseif java
--- src/hxDaedalus/graphics/TargetCanvas.hx
+++ src/hxDaedalus/graphics/TargetCanvas.hx
@@ -1,11 +1,13 @@
 package hxDaedalus.graphics;
 
-#if flambe 
+#if flambe
 typedef TargetCanvas = hxDaedalus.graphics.flambe.GraphicsComponent;
-#elseif (flash || openfl || nme) 
+#elseif (flash || openfl || nme)
 typedef TargetCanvas = flash.display.Graphics;
 #elseif luxe
 typedef TargetCanvas = Array<phoenix.geometry.Geometry>;
+#elseif svg
+typedef TargetCanvas = hxDaedalus.svg.BasicSvg;
 #elseif js
 typedef TargetCanvas = hxDaedalus.canvas.BasicCanvas;
 #elseif java
